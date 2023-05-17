a07f172857edb9c1ee0e82fe6d7a53fa42c76a3b
升高 <527518190@qq.com>
2023-05-17 17:51:16 +0800

    feat: 配置scss

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
 <template>
   <router-view></router-view>
 </template>
+
+<style lang="scss" module>
+@import "@/assets/css/index";
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import './style.css'
 import router from "@/router"
 import App from '@/App.vue'
 import "element-plus/es/components/message/style/css";
