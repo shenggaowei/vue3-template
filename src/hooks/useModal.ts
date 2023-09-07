703938b3a55e73eb4620b40aa67a29063ed269b6
shengGao <527518190@qq.com>
2023-09-07 14:49:12 +0800

    fix: update usemodal

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,29 +1,29 @@
-import { ref } from 'vue';
+import { ref, reactive } from "vue";
 
-function useModal<T = any>() {
-    const visible = ref<boolean>(false);
-    const currentItem = ref<T>();
+const useModal = <T = undefined>() => {
+  const visible = ref<boolean>(false);
+  const currentItem = ref<T>();
 
-    const open = (item?: T) => {
-        visible.value = true;
-        if (item) {
-            currentItem.value = item;
-        }
-    };
+  const open = (item?: T) => {
+    visible.value = true;
+    if (item) {
+      currentItem.value = item;
+    }
+  };
 
-    const close = () => {
-        visible.value = false;
-        currentItem.value = undefined;
-    };
+  const close = () => {
+    visible.value = false;
+    currentItem.value = undefined;
+  };
 
-    return {
-        open,
-        close,
-        visible,
-        currentItem
-    };
-}
+  return reactive({
+    open,
+    close,
+    visible,
+    currentItem,
+  });
+};
 
-export type TUseModal = ReturnType<typeof useModal<any>>;
+export type TUseModal = ReturnType<typeof useModal<undefined>>;
 
 export default useModal;
